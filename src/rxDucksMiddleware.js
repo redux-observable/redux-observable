ad85bcb1b18a8a21c97e4162efc6c04b0deb6fe4
Ben Lesh <blesh@netflix.com>
2016-04-22 13:23:30 -0700

    refactor(rxDucksMiddleware): change API to use transformer function
    BREAKING CHANGE: This completely changes the API of the middleware. It now just returns a middleware function and uses a transformer to wire up async changes

--- src/rxDucksMiddleware.js
+++ src/rxDucksMiddleware.js
@@ -1,23 +1,33 @@
 import { Subject } from 'rxjs/Subject';
 
-/**
- * Creates middleware as well as input and output subjects that can be used to transform
- * actions in Redux. Intercepts actions and sends them to an output subject. Also hooks
- * the `next` middleware and provides an input subject
- * @returns {Object} a hash of `actions`, `send` and `middlware`.
- * - `actions`: An observable of actions inbound to the middleware
- * - `middleware`: The middlware handler itself, this is what you register with Redux
- * - `send`: An observer to send actions in order to continue processing the action
- */
-export function rxDucksMiddleware() {
-  const actions = new Subject();
-  const send = new Subject();
-  const middleware = (store) => (next) => {
-    send.subscribe(next);
-    return (action) => {
-      actions.next(action);
+export function rxDucksMiddleware(transform) {
+  let actions = new Subject();
+  let send = new Subject();
+  let subscription;
+
+  let middleware = (store) => {
+    return (next) => {
+      send.subscribe(next);
+      return (action) => {
+        actions.next(action);
+      };
     };
   };
 
-  return { actions, send, middleware };
+  middleware.connect = () => {
+    if (!subscription) {
+      let transformer = transform(actions);
+      subscription = transformer.subscribe(action => send.next(action));
+    }
+    return subscription;
+  };
+
+  middleware.unsubscribe = () => {
+    if (subscription) {
+      subscription.unsubscribe();
+      subscription = null;
+    }
+  };
+
+  return middleware;
 }
