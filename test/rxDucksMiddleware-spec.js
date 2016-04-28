7a757861bc8f7dc1c20fe0cd05ff564f169c9768
Ben Lesh <blesh@netflix.com>
2016-04-27 20:49:33 -0700

    refactor(rxDucksMiddleware): completely change all the things
    BREAKING CHANGE: Completely revamped usage model. Now you're expected to dispatch an action with an `async` property that is a function that takes the stream of all actions and returns an observable. When you  such a function, an Rx Subscription is returned as a nice-to-have, but it is probably better to compose in the termination of async operations with a `takeUntil` operator.

--- test/rxDucksMiddleware-spec.js
+++ test/rxDucksMiddleware-spec.js
@@ -2,67 +2,48 @@
 import { expect } from 'chai';
 import { createStore, applyMiddleware } from 'redux';
 import { rxDucksMiddleware } from '../';
+import { timer } from 'rxjs/observable/timer';
 import { map } from 'rxjs/operator/map';
+import { startWith } from 'rxjs/operator/startWith';
 
 describe('rxDucksMiddleware', () => {
   it('should exist', () => {
     expect(rxDucksMiddleware).to.be.a('function');
   });
 
-  it('should intercept and process actions', () => {
-    const middleware = rxDucksMiddleware((actions) => actions::map(x => ({ ...x, data: x.data + '!' })));
+  it('should intercept and process actions', (done) => {
+    const middleware = rxDucksMiddleware();
     const reducedActions = [];
 
-    const store = createStore((state = { data: 'start' }, action) => {
+    const store = createStore((state = {}, action) => {
       reducedActions.push(action);
+      switch (action.type) {
+        case 'STARTING_TEST':
+          return { ...state, testStarted: true };
+        case 'END_TEST':
+          expect(state.testStarted).to.equal(true);
+          expect(reducedActions).to.deep.equal([
+            { type: '@@redux/INIT' },
+            { type: 'STARTING_TEST' },
+            { type: 'END_TEST' }
+          ]);
+          done();
+          return { ...state, testEnded: true };
+        default:
+          break;
+      }
       return state;
     }, applyMiddleware(middleware));
 
-    middleware.connect();
+    let sub = store.dispatch({
+      type: 'START_TEST',
+      async: () =>
+        timer(10)
+        ::map(() => ({ type: 'END_TEST' }))
+        ::startWith({ type: 'STARTING_TEST' })
+    });
 
-    store.dispatch({ type: 'TEST', data: 'this' });
-    store.dispatch({ type: 'TEST', data: 'should' });
-    store.dispatch({ type: 'TEST', data: 'work' });
-
-    expect(reducedActions).to.deep.equal([
-      { type: '@@redux/INIT' },
-      { type: 'TEST', data: 'this' },
-      { type: 'TEST', data: 'this!' },
-      { type: 'TEST', data: 'should' },
-      { type: 'TEST', data: 'should!' },
-      { type: 'TEST', data: 'work' },
-      { type: 'TEST', data: 'work!' }
-    ]);
-
-    middleware.unsubscribe();
-
-    store.dispatch({ type: 'NOT_HANDLED', data: '' });
-
-    expect(reducedActions).to.deep.equal([
-      { type: '@@redux/INIT' },
-      { type: 'TEST', data: 'this' },
-      { type: 'TEST', data: 'this!' },
-      { type: 'TEST', data: 'should' },
-      { type: 'TEST', data: 'should!' },
-      { type: 'TEST', data: 'work' },
-      { type: 'TEST', data: 'work!' },
-      { type: 'NOT_HANDLED', data: '' }
-    ]);
-
-    middleware.connect();
-
-    store.dispatch({ type: 'LAST_ONE', data: 'one for the road' });
-    expect(reducedActions).to.deep.equal([
-      { type: '@@redux/INIT' },
-      { type: 'TEST', data: 'this' },
-      { type: 'TEST', data: 'this!' },
-      { type: 'TEST', data: 'should' },
-      { type: 'TEST', data: 'should!' },
-      { type: 'TEST', data: 'work' },
-      { type: 'TEST', data: 'work!' },
-      { type: 'NOT_HANDLED', data: '' },
-      { type: 'LAST_ONE', data: 'one for the road' },
-      { type: 'LAST_ONE', data: 'one for the road!' }
-    ]);
+    expect(sub).to.be.a('object');
+    expect(sub.unsubscribe).to.be.a('function');
   });
 });
