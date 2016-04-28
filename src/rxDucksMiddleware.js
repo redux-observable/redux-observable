ed247ce46998da49b1fb969811d79aa22ba292dd
Ben Lesh <blesh@netflix.com>
2016-04-27 20:49:33 -0700

    refactor(rxDucksMiddleware): completely change all the things
    BREAKING CHANGE: Completely revamped usage model. Now you're expected to dispatch an action with an `async` property that is a function that takes the stream of all actions and returns an observable. When you  such a function, an Rx Subscription is returned as a nice-to-have, but it is probably better to compose in the termination of async operations with a `takeUntil` operator.

--- src/rxDucksMiddleware.js
+++ src/rxDucksMiddleware.js
@@ -2,32 +2,14 @@ import { Subject } from 'rxjs/Subject';
 
 export function rxDucksMiddleware(transform) {
   let actions = new Subject();
-  let send = new Subject();
-  let subscription;
-
-  let middleware = (store) => {
-    return (next) => {
-      send.subscribe(next);
-      return (action) => {
-        next(action);
-        actions.next(action);
-      };
+  let middleware = (store) => (next) => {
+    return (action) => {
+      if (typeof action.async === 'function') {
+        let observable = action.async(actions);
+        return observable.subscribe(next);
+      }
+      return next(action);
     };
-  };
-
-  middleware.connect = () => {
-    if (!subscription) {
-      let transformer = transform(actions);
-      subscription = transformer.subscribe(action => send.next(action));
-    }
-    return subscription;
-  };
-
-  middleware.unsubscribe = () => {
-    if (subscription) {
-      subscription.unsubscribe();
-      subscription = null;
-    }
   };
 
   return middleware;
