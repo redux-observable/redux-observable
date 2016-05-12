7f0971526d0d9c759e88762b515030a79ca77fe9
Jay Phelps <hello@jayphelps.com>
2016-05-12 14:37:14 -0700

    docs(examples): add examples dir with a basic one

---
+++ examples/basic/src/reducers/index.js
@@ -0,0 +1,20 @@
+import { combineReducers } from 'redux';
+import { FETCH_USER_PENDING, FETCH_USER_FULFILLED, FETCH_USER_ABORTED } from '../actions';
+
+const user = (state = { isLoading: false }, action) => {
+  switch (action.type) {
+    case FETCH_USER_PENDING:
+      return { ...state, isLoading: true };
+
+    case FETCH_USER_FULFILLED:
+      return { ...state, isLoading: false, ...action.payload };
+
+    case FETCH_USER_ABORTED:
+      return { ...state, isLoading: false };
+
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({ user });
