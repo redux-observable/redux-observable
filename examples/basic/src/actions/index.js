8438b5b66006fd77b746497729a7ae78b7c08613
Jay Phelps <hello@jayphelps.com>
2016-06-07 21:44:57 -0700

    chore(examples/basic): indent style changes..

--- examples/basic/src/actions/index.js
+++ examples/basic/src/actions/index.js
@@ -3,19 +3,14 @@ import * as Rx from 'rxjs';
 export const FETCH_USER_PENDING = 'FETCH_USER_PENDING';
 export const FETCH_USER_FULFILLED = 'FETCH_USER_FULFILLED';
 export const FETCH_USER_ABORTED = 'FETCH_USER_ABORTED';
-const BASE_URL = 'http://localhost:3000/user/';
 
 export const fetchUser = () => (
-  (actions, store) => Rx.Observable.ajax.getJSON(BASE_URL)
+  (actions, store) => Rx.Observable.ajax.getJSON('http://localhost:3000/user')
     // When our request comes back, we transform it into an action
     // which the redux-observable middleware will then dispatch
-    .map(
-      payload => ({ type: FETCH_USER_FULFILLED, payload })
-    )
+    .map(payload => ({ type: FETCH_USER_FULFILLED, payload }))
     // Abort fetching the user if someone dispatches an abort action
-    .takeUntil(
-      actions.ofType(FETCH_USER_ABORTED)
-    )
+    .takeUntil(actions.ofType(FETCH_USER_ABORTED))
     // Let's us immediately update the user's state so we can display
     // loading messages to the user, etc.
     .startWith({ type: FETCH_USER_PENDING })
