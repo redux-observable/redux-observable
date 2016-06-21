3e9de66c80432595bc8bb4faaadf5f842954c4e2
cybice <istarkov@gmail.com>
2016-06-22 02:13:47 +0300

    support of npmcdn

---
+++ webpack.config.babel.js
@@ -0,0 +1,36 @@
+import webpack from 'webpack';
+
+const env = process.env.NODE_ENV;
+
+const config = {
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
+    ]
+  },
+  output: {
+    library: 'ReduxObservable',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    })
+  ]
+};
+
+if (env === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
+    })
+  );
+}
+
+export default config;
