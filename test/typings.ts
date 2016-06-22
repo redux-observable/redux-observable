994c1a58c240e457865496a5ed2fce8a302c2b25
cybice <istarkov@gmail.com>
2016-06-22 09:07:19 +0300

    Fix typings to support processor.

--- test/typings.ts
+++ test/typings.ts
@@ -1,9 +1,20 @@
-import { reduxObservable, ActionsObservable } from '../index';
+import { reduxObservable, ActionsObservable, Processor } from '../index';
 import { Observable } from 'rxjs/Observable';
-
+interface Action {
+  type: string
+}
+interface ReduxStateShape {
+  [key: string]: any
+}
 // should be a function
 reduxObservable();
 
 // should be a constructor that returns an observable
 const actionsSubject = Observable.create(() => {});
 const aoTest: Observable<string> = new ActionsObservable(actionsSubject);
+
+const processor: Processor<Action, ReduxStateShape> =
+  (actions$, { dispatch }) => actions$.ofType('A_TYPE');
+
+// should accept processor
+reduxObservable(processor);
