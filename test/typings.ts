9697ca87c696e72f7ff9ba2cfa5f69c35686cce0
Jay Phelps <hello@jayphelps.com>
2016-07-13 17:29:49 -0700

    fix(TypeScript type definition): Add combineEpics(), provide more accurate type info for others fixes #69

--- test/typings.ts
+++ test/typings.ts
@@ -1,9 +1,25 @@
-import { createEpicMiddleware, ActionsObservable } from '../index';
+import { createEpicMiddleware, combineEpics, ActionsObservable } from '../index';
+import { Action } from 'redux';
 import { Observable } from 'rxjs/Observable';
 
-// should be a function
-createEpicMiddleware();
+const epic1 = (action$, store) =>
+  action$.ofType('FIRST')
+    .mapTo({
+      type: 'first',
+      payload: store.getState()
+    });
+
+const epic2 = (action$, store) =>
+  action$.ofType('SECOND')
+    .mapTo({
+      type: 'second',
+      payload: store.getState()
+    });
+
+const rootEpic = combineEpics(epic1, epic2);
+
+createEpicMiddleware(rootEpic);
 
 // should be a constructor that returns an observable
 const actionsSubject = Observable.create(() => {});
-const aoTest: Observable<string> = new ActionsObservable(actionsSubject);
+const aoTest: Observable<Action> = new ActionsObservable(actionsSubject);
