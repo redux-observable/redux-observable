334f1601e58a9198047f243b4e56c09d3d03a9cc
Jay Phelps <hello@jayphelps.com>
2016-07-15 23:47:05 -0700

    feat(replaceEpic): Dispatches an EPIC_END action when you replaceEpic() so that you can listen for it and emit any state cleanup

--- src/createEpicMiddleware.js
+++ src/createEpicMiddleware.js
@@ -1,35 +1,44 @@
 import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { from } from 'rxjs/observable/from';
+import { switchMap } from 'rxjs/operator/switchMap';
 import { ActionsObservable } from './ActionsObservable';
+import { EPIC_END } from './EPIC_END';
 
 export function createEpicMiddleware(epic) {
   const actionsSubject = new Subject();
   const action$ = new ActionsObservable(actionsSubject);
   const epic$ = new BehaviorSubject(epic);
+  let store;
 
-  const epicMiddleware = store => next => {
-    epic$.filter(epic => typeof epic === 'function')
-      .switchMap(epic => epic(action$, store))
-      .subscribe(store.dispatch);
+  const epicMiddleware = _store => {
+    store = _store;
 
-    return action => {
-      if (typeof action === 'function') {
-        if (typeof console !== 'undefined' && typeof console.warn !== 'undefined') {
-          console.warn('DEPRECATION: Using thunkservables with redux-observable is now deprecated in favor of the new "Epics" feature. See http://redux-observable.js.org/docs/FAQ.html#why-were-thunkservables-deprecated');
-        }
-
-        const out$ = from(action(action$, store));
-        return out$.subscribe(store.dispatch);
-      } else {
-        const result = next(action);
-        actionsSubject.next(action);
-        return result;
+    return next => {
+      if (typeof epic === 'function') {
+        epic$::switchMap(epic => epic(action$, store))
+          .subscribe(store.dispatch);
       }
+
+      return action => {
+        if (typeof action === 'function') {
+          if (typeof console !== 'undefined' && typeof console.warn !== 'undefined') {
+            console.warn('DEPRECATION: Using thunkservables with redux-observable is now deprecated in favor of the new "Epics" feature. See http://redux-observable.js.org/docs/FAQ.html#why-were-thunkservables-deprecated');
+          }
+
+          const out$ = from(action(action$, store));
+          return out$.subscribe(store.dispatch);
+        } else {
+          const result = next(action);
+          actionsSubject.next(action);
+          return result;
+        }
+      };
     };
   };
 
   epicMiddleware.replaceEpic = epic => {
+    store.dispatch({ type: EPIC_END });
     epic$.next(epic);
   };
 
