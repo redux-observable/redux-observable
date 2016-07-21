baadc31767e30183b6a71a2c4d1fd861d4f33e0c
Jay Phelps <hello@jayphelps.com>
2016-07-20 22:56:34 -0700

    chore(gitbook): adjust gitbook website style so window scrollbar is used, not an inner overflow one This makes it easier to use the website and doesn't prevent scrolling when hovering over the left/right arrow buttons on the sides of content

--- website/script.js
+++ website/script.js
@@ -1,7 +1,15 @@
 window.gitbook = window.gitbook || [];
 
 gitbook.push(function () {
+
   gitbook.events.on('page.change', function () {
+    // We use a slightly different styling so that
+    // the normal window scrollbar is used, so we need
+    // to reset the scroll position every time.
+    // We delay it because gitbook theme does some
+    // toolbar stuff we have to wait for..#YOLO
+    window.scrollTo(0, 0);
+
     // JSbin embed code doesn't work with as-is with
     // single page apps. When you navigate to another
     // page it doesn't hydrate those new embeds.
