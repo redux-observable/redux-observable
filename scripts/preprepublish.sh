e412542a80ab5a5c450742983af95b5388d72af8
Jay Phelps <hello@jayphelps.com>
2016-07-23 21:53:25 -0700

    chore(shipit): also push master after cutting a release

--- scripts/preprepublish.sh
+++ scripts/preprepublish.sh
@@ -11,6 +11,7 @@ if [[ -z $(git status -uno --porcelain) ]]; then
     git add package.json CHANGELOG.md;
     git commit -m "chore(publish): ${VERSION}";
     git tag "${VERSION}";
+    git push origin master;
     git push origin "${VERSION}";
   else
     git checkout -f package.json CHANGELOG.md;
