b8fa4d3b205d3204a873634ab5bf2a7c1a625d1a
Gus Vargas <gvargas@hubspot.com>
2016-08-02 17:07:08 -0400

    chore(.js -> .jsx): Fix syntax highlighting in GH

--- examples/navigation/webpack.config.js
+++ examples/navigation/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: 'babel',
         query: {
@@ -18,5 +18,8 @@ module.exports = {
         }
       }
     ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
   }
 };
