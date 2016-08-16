59015e359bd0d267e17699b9c2e6f163631d2d4c
Gus Vargas <gvargas@hubspot.com>
2016-08-16 01:30:13 -0400

    fix(readbility): start new observable on new line

--- examples/navigation/epics/searchUsers.js
+++ examples/navigation/epics/searchUsers.js
@@ -9,13 +9,14 @@ export default function searchUsers(action$) {
     .map(action => action.payload.query)
     .filter(q => !!q);
   return searchIntents$
-    .switchMap(q => Observable.timer(800)
-      .takeUntil(action$.ofType(ActionTypes.CLEARED_SEARCH_RESULTS))
-      .mergeMap(() => Observable.merge(
-        Observable.of(replace(`?q=${q}`)),
-        ajax.getJSON(`https://api.github.com/search/users?q=${q}`)
-          .map(res => res.items)
-          .map(receiveUsers)
-      ))
+    .switchMap(q =>
+      Observable.timer(800) // debounce
+        .takeUntil(action$.ofType(ActionTypes.CLEARED_SEARCH_RESULTS))
+        .mergeMap(() => Observable.merge(
+          Observable.of(replace(`?q=${q}`)),
+          ajax.getJSON(`https://api.github.com/search/users?q=${q}`)
+            .map(res => res.items)
+            .map(receiveUsers)
+        ))
     );
 };
