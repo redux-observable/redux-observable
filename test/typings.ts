b6709c0e86305ae734b3f276c5782638a80c8ef5
Jay Phelps <hello@jayphelps.com>
2016-08-30 13:50:20 -0700

    chore(testing): add more complete testing for TypeScript

--- test/typings.ts
+++ test/typings.ts
@@ -1,9 +1,10 @@
 import { createEpicMiddleware, Epic, combineEpics,
   EpicMiddleware, ActionsObservable } from '../index';
-import { Action } from 'redux';
+import { Action, createStore, applyMiddleware } from 'redux';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/mapTo';
+import 'rxjs/add/operator/mergeMap';
 
 const epic1: Epic = (action$, store) =>
   action$.ofType('FIRST')
@@ -14,10 +15,8 @@ const epic1: Epic = (action$, store) =>
 
 const epic2: Epic = (action$, store) =>
   action$.ofType('SECOND')
-    .mapTo({
-      type: 'second',
-      payload: store.getState()
-    });
+    .mapTo('second')
+    .mergeMap(type => Observable.of({ type }));
 
 const epic3: Epic = action$ =>
   action$.ofType('THIRD')
@@ -27,7 +26,7 @@ const epic3: Epic = action$ =>
 
 const epic4: Epic = () =>
   Observable.of({
-    type: 'third'
+    type: 'fourth'
   });
 
 const rootEpic1: Epic = combineEpics(epic1, epic2, epic3, epic4);
@@ -40,3 +39,12 @@ epicMiddleware.replaceEpic(rootEpic2);
 // should be a constructor that returns an observable
 const actionsSubject = Observable.create(() => {});
 const aoTest: Observable<Action> = new ActionsObservable(actionsSubject);
+
+const reducer = (state = [], action) => state.concat(action);
+const store = createStore(
+  reducer,
+  applyMiddleware(epicMiddleware)
+);
+
+store.dispatch({ type: 'FIRST' });
+store.dispatch({ type: 'SECOND' });
