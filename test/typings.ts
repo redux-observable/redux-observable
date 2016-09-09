15ec2ad135350aeeda51598c6d0e0838aff47e6b
Jay Phelps <hello@jayphelps.com>
2016-09-09 14:37:12 -0700

    fix(typings): Add generics so developer defines what redux Actions look like

--- test/typings.ts
+++ test/typings.ts
@@ -1,50 +1,76 @@
 import { createEpicMiddleware, Epic, combineEpics,
   EpicMiddleware, ActionsObservable } from '../index';
-import { Action, createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/mapTo';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 
-const epic1: Epic = (action$, store) =>
+interface FluxStandardAction {
+  type: string | symbol | any;
+  payload?: any;
+  error?: boolean | any;
+  meta?: any
+}
+
+const epic1: Epic<FluxStandardAction> = (action$: ActionsObservable<FluxStandardAction>, store) =>
   action$.ofType('FIRST')
     .mapTo({
       type: 'first',
       payload: store.getState()
     });
 
-const epic2: Epic = (action$, store) =>
-  action$.ofType('SECOND')
+const epic2: Epic<FluxStandardAction> = (action$, store) =>
+  action$.ofType('SECOND', 'NEVER')
     .mapTo('second')
     .mergeMap(type => Observable.of({ type }));
 
-const epic3: Epic = action$ =>
+const epic3: Epic<FluxStandardAction> = action$ =>
   action$.ofType('THIRD')
     .mapTo({
       type: 'third'
     });
 
-const epic4: Epic = () =>
+const epic4: Epic<FluxStandardAction> = () =>
   Observable.of({
     type: 'fourth'
   });
 
-const rootEpic1: Epic = combineEpics(epic1, epic2, epic3, epic4);
-const rootEpic2: Epic = combineEpics(epic1, epic2, epic3, epic4);
+const epic5: Epic<FluxStandardAction> = (action$, store) =>
+  action$.ofType('FIFTH')
+    .flatMap(({ type, payload }) => Observable.of({
+      type: 'fifth',
+      payload
+    }));
 
-const epicMiddleware: EpicMiddleware = createEpicMiddleware(rootEpic1);
+const epic6 = (action$, store) =>
+  action$.ofType('SIXTH')
+    .map(({ type, payload }) => ({
+      type: 'sixth',
+      payload
+    }));
 
-epicMiddleware.replaceEpic(rootEpic2);
+const rootEpic1: Epic<FluxStandardAction> = combineEpics<FluxStandardAction>(epic1, epic2, epic3, epic4, epic5, epic6);
+const rootEpic2 = combineEpics(epic1, epic2, epic3, epic4, epic5, epic6);
+
+const epicMiddleware1: EpicMiddleware<FluxStandardAction> = createEpicMiddleware<FluxStandardAction>(rootEpic1);
+const epicMiddleware2 = createEpicMiddleware(rootEpic2);
+
+epicMiddleware1.replaceEpic(rootEpic2);
+epicMiddleware2.replaceEpic(rootEpic1)
 
 // should be a constructor that returns an observable
-const actionsSubject = Observable.create(() => {});
-const aoTest: Observable<Action> = new ActionsObservable(actionsSubject);
+const input$ = Observable.create(() => {});
+const action$: ActionsObservable<FluxStandardAction> = new ActionsObservable<FluxStandardAction>(input$);
 
 const reducer = (state = [], action) => state.concat(action);
 const store = createStore(
   reducer,
-  applyMiddleware(epicMiddleware)
+  applyMiddleware(epicMiddleware1, epicMiddleware2)
 );
 
 store.dispatch({ type: 'FIRST' });
 store.dispatch({ type: 'SECOND' });
+store.dispatch({ type: 'FIFTH', payload: 'fifth-payload' });
+store.dispatch({ type: 'SIXTH', payload: 'sixth-payload' });
