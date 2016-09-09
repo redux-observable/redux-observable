065be61a15c0c739efd9872c3c547c8ac31f1a0f
Jay Phelps <hello@jayphelps.com>
2016-09-09 15:42:49 -0700

    chore(typings): actualy run the typings.js file to confirm it works right

--- test/typings.ts
+++ test/typings.ts
@@ -1,11 +1,13 @@
-import { createEpicMiddleware, Epic, combineEpics,
-  EpicMiddleware, ActionsObservable } from '../index';
+import { expect } from 'chai';
 import { createStore, applyMiddleware } from 'redux';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/mapTo';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
+
+import { createEpicMiddleware, Epic, combineEpics,
+  EpicMiddleware, ActionsObservable } from '../';
 
 interface FluxStandardAction {
   type: string | symbol | any;
@@ -57,9 +59,6 @@ const rootEpic2 = combineEpics(epic1, epic2, epic3, epic4, epic5, epic6);
 const epicMiddleware1: EpicMiddleware<FluxStandardAction> = createEpicMiddleware<FluxStandardAction>(rootEpic1);
 const epicMiddleware2 = createEpicMiddleware(rootEpic2);
 
-epicMiddleware1.replaceEpic(rootEpic2);
-epicMiddleware2.replaceEpic(rootEpic1)
-
 // should be a constructor that returns an observable
 const input$ = Observable.create(() => {});
 const action$: ActionsObservable<FluxStandardAction> = new ActionsObservable<FluxStandardAction>(input$);
@@ -70,7 +69,50 @@ const store = createStore(
   applyMiddleware(epicMiddleware1, epicMiddleware2)
 );
 
+epicMiddleware1.replaceEpic(rootEpic2);
+epicMiddleware2.replaceEpic(rootEpic1)
+
 store.dispatch({ type: 'FIRST' });
 store.dispatch({ type: 'SECOND' });
 store.dispatch({ type: 'FIFTH', payload: 'fifth-payload' });
 store.dispatch({ type: 'SIXTH', payload: 'sixth-payload' });
+
+expect(store.getState()).to.deep.equal([
+  { "type": "@@redux/INIT" },
+  { "type": "fourth" },
+  { "type": "fourth" },
+  { "type": "@@redux-observable/EPIC_END" },
+  { "type": "fourth" },
+  { "type": "@@redux-observable/EPIC_END" },
+  { "type": "fourth" },
+  { "type": "FIRST" },
+  { "type": "first",
+    "payload": [
+      { "type": "@@redux/INIT" },
+      { "type": "fourth" },
+      { "type": "fourth" },
+      { "type": "@@redux-observable/EPIC_END" },
+      { "type": "fourth" },
+      { "type": "@@redux-observable/EPIC_END" }
+    ]
+  },
+  { "type": "first",
+    "payload": [
+      { "type": "@@redux/INIT" },
+      { "type": "fourth" },
+      { "type": "fourth" },
+      { "type": "@@redux-observable/EPIC_END" }
+    ]
+  },
+  { "type": "SECOND" },
+  { "type": "second" },
+  { "type": "second" },
+  { "type": "FIFTH", "payload": "fifth-payload" },
+  { "type": "fifth", "payload": "fifth-payload" },
+  { "type": "fifth", "payload": "fifth-payload" },
+  { "type": "SIXTH", "payload": "sixth-payload" },
+  { "type": "sixth", "payload": "sixth-payload" },
+  { "type": "sixth", "payload": "sixth-payload" }
+]);
+
+console.log('typings.ts: OK');
