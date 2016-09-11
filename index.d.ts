7b4214fb7dad28aa125a3545380b4e401cb6fa0e
Jay Phelps <hello@jayphelps.com>
2016-09-10 19:18:34 -0700

    fix(typings): Add generics so developer defines what redux Actions look like (#105)
    When using TypeScript, you can now provide the expected action type by passing it as the first generic in `createEpicMiddleware<Action>(rootEpic)`. Most likely you'll use @types/flux-standard-action

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,21 @@
-import { Middleware, MiddlewareAPI, Action } from 'redux';
+import { Middleware, MiddlewareAPI } from 'redux';
 import { Observable } from 'rxjs/Observable';
 import { Operator } from 'rxjs/Operator';
 
-export declare class ActionsObservable extends Observable<Action> {
-  constructor(actionsSubject: Observable<Action>);
-  lift(operator: Operator<any, Action>) : ActionsObservable;
-  ofType(...key: any[]) : ActionsObservable;
+export declare class ActionsObservable<T> extends Observable<T> {
+  constructor(input$: Observable<T>);
+  lift(operator: Operator<any, T>) : ActionsObservable<T>;
+  ofType(...key: any[]) : ActionsObservable<T>;
 }
 
-export declare interface Epic {
-  (action$: ActionsObservable, store: MiddlewareAPI<any>): Observable<Action>;
+export declare interface Epic<T> {
+  (action$: ActionsObservable<T>, store: MiddlewareAPI<any>): Observable<T>;
 }
 
-export interface EpicMiddleware extends Middleware {
-  replaceEpic(nextEpic: Epic): void;
+export interface EpicMiddleware<T> extends Middleware {
+  replaceEpic(nextEpic: Epic<T>): void;
 }
 
-export declare function createEpicMiddleware(rootEpic: Epic): EpicMiddleware;
+export declare function createEpicMiddleware<T>(rootEpic: Epic<T>): EpicMiddleware<T>;
 
-export declare function combineEpics(...epics: Epic[]): Epic;
+export declare function combineEpics<T>(...epics: Epic<T>[]): Epic<T>;
--- test/typings.ts
+++ test/typings.ts
@@ -1,50 +1,118 @@
-import { createEpicMiddleware, Epic, combineEpics,
-  EpicMiddleware, ActionsObservable } from '../index';
-import { Action, createStore, applyMiddleware } from 'redux';
+import { expect } from 'chai';
+import { createStore, applyMiddleware } from 'redux';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/mapTo';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 
-const epic1: Epic = (action$, store) =>
+import { createEpicMiddleware, Epic, combineEpics,
+  EpicMiddleware, ActionsObservable } from '../';
+
+interface FluxStandardAction {
+  type: string | symbol | any;
+  payload?: any;
+  error?: boolean | any;
+  meta?: any
+}
+
+const epic1: Epic<FluxStandardAction> = (action$: ActionsObservable<FluxStandardAction>, store) =>
   action$.ofType('FIRST')
     .mapTo({
       type: 'first',
       payload: store.getState()
     });
 
-const epic2: Epic = (action$, store) =>
-  action$.ofType('SECOND')
+const epic2: Epic<FluxStandardAction> = (action$, store) =>
+  action$.ofType('SECOND', 'NEVER')
     .mapTo('second')
     .mergeMap(type => Observable.of({ type }));
 
-const epic3: Epic = action$ =>
+const epic3: Epic<FluxStandardAction> = action$ =>
   action$.ofType('THIRD')
     .mapTo({
       type: 'third'
     });
 
-const epic4: Epic = () =>
+const epic4: Epic<FluxStandardAction> = () =>
   Observable.of({
     type: 'fourth'
   });
 
-const rootEpic1: Epic = combineEpics(epic1, epic2, epic3, epic4);
-const rootEpic2: Epic = combineEpics(epic1, epic2, epic3, epic4);
+const epic5: Epic<FluxStandardAction> = (action$, store) =>
+  action$.ofType('FIFTH')
+    .flatMap(({ type, payload }) => Observable.of({
+      type: 'fifth',
+      payload
+    }));
 
-const epicMiddleware: EpicMiddleware = createEpicMiddleware(rootEpic1);
+const epic6 = (action$, store) =>
+  action$.ofType('SIXTH')
+    .map(({ type, payload }) => ({
+      type: 'sixth',
+      payload
+    }));
 
-epicMiddleware.replaceEpic(rootEpic2);
+const rootEpic1: Epic<FluxStandardAction> = combineEpics<FluxStandardAction>(epic1, epic2, epic3, epic4, epic5, epic6);
+const rootEpic2 = combineEpics(epic1, epic2, epic3, epic4, epic5, epic6);
+
+const epicMiddleware1: EpicMiddleware<FluxStandardAction> = createEpicMiddleware<FluxStandardAction>(rootEpic1);
+const epicMiddleware2 = createEpicMiddleware(rootEpic2);
 
 // should be a constructor that returns an observable
-const actionsSubject = Observable.create(() => {});
-const aoTest: Observable<Action> = new ActionsObservable(actionsSubject);
+const input$ = Observable.create(() => {});
+const action$: ActionsObservable<FluxStandardAction> = new ActionsObservable<FluxStandardAction>(input$);
 
 const reducer = (state = [], action) => state.concat(action);
 const store = createStore(
   reducer,
-  applyMiddleware(epicMiddleware)
+  applyMiddleware(epicMiddleware1, epicMiddleware2)
 );
+
+epicMiddleware1.replaceEpic(rootEpic2);
+epicMiddleware2.replaceEpic(rootEpic1)
 
 store.dispatch({ type: 'FIRST' });
 store.dispatch({ type: 'SECOND' });
+store.dispatch({ type: 'FIFTH', payload: 'fifth-payload' });
+store.dispatch({ type: 'SIXTH', payload: 'sixth-payload' });
+
+expect(store.getState()).to.deep.equal([
+  { "type": "@@redux/INIT" },
+  { "type": "fourth" },
+  { "type": "fourth" },
+  { "type": "@@redux-observable/EPIC_END" },
+  { "type": "fourth" },
+  { "type": "@@redux-observable/EPIC_END" },
+  { "type": "fourth" },
+  { "type": "FIRST" },
+  { "type": "first",
+    "payload": [
+      { "type": "@@redux/INIT" },
+      { "type": "fourth" },
+      { "type": "fourth" },
+      { "type": "@@redux-observable/EPIC_END" },
+      { "type": "fourth" },
+      { "type": "@@redux-observable/EPIC_END" }
+    ]
+  },
+  { "type": "first",
+    "payload": [
+      { "type": "@@redux/INIT" },
+      { "type": "fourth" },
+      { "type": "fourth" },
+      { "type": "@@redux-observable/EPIC_END" }
+    ]
+  },
+  { "type": "SECOND" },
+  { "type": "second" },
+  { "type": "second" },
+  { "type": "FIFTH", "payload": "fifth-payload" },
+  { "type": "fifth", "payload": "fifth-payload" },
+  { "type": "fifth", "payload": "fifth-payload" },
+  { "type": "SIXTH", "payload": "sixth-payload" },
+  { "type": "sixth", "payload": "sixth-payload" },
+  { "type": "sixth", "payload": "sixth-payload" }
+]);
+
+console.log('typings.ts: OK');
