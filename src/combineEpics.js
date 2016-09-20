ee3efbfe2da921303f758a40570a2b794b8bbedf
Jay Phelps <hello@jayphelps.com>
2016-09-20 12:36:44 -0700

    feat(combineEpics): combineEpics() now transparently passes along _any_ arguments, not just action$, store.

--- src/combineEpics.js
+++ src/combineEpics.js
@@ -3,5 +3,7 @@ import { merge } from 'rxjs/observable/merge';
 /**
   Merges all epics into a single one.
  */
-export const combineEpics = (...epics) => (actions, store) =>
-  merge(...(epics.map(epic => epic(actions, store))));
+export const combineEpics = (...epics) => (...args) =>
+  merge(
+    ...epics.map(epic => epic(...args))
+  );
