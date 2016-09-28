338423788d8430cc86f0286ad0d4ae5aa20d4010
Jay Phelps <hello@jayphelps.com>
2016-09-28 14:10:40 -0700

    chore(examples): use babelrc: false so example doesn't pickup parent dir .babelrc's
    Fixes #123

--- examples/navigation/webpack.config.js
+++ examples/navigation/webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015', 'react'],
+          babelrc: false
         }
       }
     ]
