c3d56ddd6d34ef5a73f99bcad7e1bfb017b16406
Clayton Ray <iamclaytonray@gmail.com>
2017-09-15 15:35:14 -0400

    docs(examples): Remove deprecated React.PropTypes in favor of the prop-types package (#327)

--- examples/redux-observable-cancellable-counter/js/components/Counter.js
+++ examples/redux-observable-cancellable-counter/js/components/Counter.js
@@ -1,5 +1,6 @@
 /*eslint-disable no-unused-vars*/
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 import {
