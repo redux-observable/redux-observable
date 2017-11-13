1d99dbdbff832c5be3186fdff479c9fc7262344b
Łukasz Sentkiewicz <sirmims@gmail.com>
2017-11-13 15:50:38 +0100

    refactor(ofType): simplify ofType by using Array.some
    Signed-off-by: Łukasz Sentkiewicz <sirmims@gmail.com>

--- src/operators.js
+++ src/operators.js
@@ -1,23 +1,11 @@
 import { filter } from 'rxjs/operator/filter';
 
-const keyHasType = (type, key) => {
+const keyHasType = type => key => {
   return type === key || typeof key === 'function' && type === key.toString();
 };
 
 export function ofType(...keys) {
   return function ofTypeOperatorFunction(source) {
-    return source::filter(({ type }) => {
-      const len = keys.length;
-      if (len === 1) {
-        return keyHasType(type, keys[0]);
-      } else {
-        for (let i = 0; i < len; i++) {
-          if (keyHasType(type, keys[i])) {
-            return true;
-          }
-        }
-      }
-      return false;
-    });
+    return source::filter(({ type }) => keys.some(keyHasType(type)));
   };
 }
