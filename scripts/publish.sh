426dac073de3cb957028c533c8bf35553c3df739
Jay Phelps <hello@jayphelps.com>
2018-04-09 13:48:28 -0400

    chore(publishing): Correct publish bash script logic

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -18,10 +18,10 @@ if [[ -z $(git status -uno --porcelain) ]]; then
     read -p "Which dist-tag? (latest) " DIST_TAG;
     DIST_TAG=${DIST_TAG:-latest}
 
-    if [[ $BUMP =~ - && "$DIST_TAG" == "latest"]]; then
+    if [ $BUMP =~ - ] && [ "$DIST_TAG" == "latest" ]; then
       read -p "Using dist-tag 'latest' for a pre-release. ARE YOU SURE? (y/n) " CONDITION;
 
-      if [ "$CONDITION" == "n" || "$CONDITION" == "" ]; then
+      if [ "$CONDITION" == "n" ] || [ "$CONDITION" == "" ]; then
         echo "Cancelled publish by your request!";
         exit 1;
       fi
