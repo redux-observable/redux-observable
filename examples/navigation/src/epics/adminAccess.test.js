7cf97ed0d79f9bd42278669ff88403e94e13a6e0
Tane Morgan <tane.morgan@gmail.com>
2019-02-12 20:05:30 +1300

    Reword adminAccess epic test

--- examples/navigation/src/epics/adminAccess.test.js
+++ examples/navigation/src/epics/adminAccess.test.js
@@ -4,21 +4,23 @@ import { accessDenied, CHECKED_ADMIN_ACCESS } from '../actions'
 import adminAccessEpic from './adminAccess'
 
 describe('adminAccess epic', () => {
-  it('creates an accessDenied action first, then a route change action', () => {
-    const testScheduler = new TestScheduler((actual, expected) => {
-      expect(actual).toEqual(expected)
-    })
-
-    testScheduler.run(({ hot, expectObservable }) => {
-      const action$ = hot('-a', {
-        a: { type: CHECKED_ADMIN_ACCESS }
+  describe('when admin access is checked', () => {
+    it('creates actions to deny access and redirect back home', () => {
+      const testScheduler = new TestScheduler((actual, expected) => {
+        expect(actual).toEqual(expected)
       })
-      const state$ = null
-      const output$ = adminAccessEpic(action$, state$)
 
-      expectObservable(output$).toBe('- 2000ms a 1999ms b -', {
-        a: accessDenied(),
-        b: push('/')
+      testScheduler.run(({ hot, expectObservable }) => {
+        const action$ = hot('-a', {
+          a: { type: CHECKED_ADMIN_ACCESS }
+        })
+        const state$ = null
+        const output$ = adminAccessEpic(action$, state$)
+
+        expectObservable(output$).toBe('- 2000ms a 1999ms b -', {
+          a: accessDenied(),
+          b: push('/')
+        })
       })
     })
   })
