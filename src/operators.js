c033b958c63654c2d8770d2ffe5a29d4cbe244a4
mikeheddes <mikeheddes@gmail.com>
2019-04-27 15:29:07 +0200

    refactor(operators): improve readability of ofType operator

--- src/operators.js
+++ src/operators.js
@@ -1,21 +1,9 @@
 import { filter } from 'rxjs/operators';
 
-const keyHasType = (type, key) => {
+const matchesType = type => key => {
   return type === key || typeof key === 'function' && type === key.toString();
 };
 
-export const ofType = (...keys) => (source) => source.pipe(
-  filter(({ type }) => {
-    const len = keys.length;
-    if (len === 1) {
-      return keyHasType(type, keys[0]);
-    } else {
-      for (let i = 0; i < len; i++) {
-        if (keyHasType(type, keys[i])) {
-          return true;
-        }
-      }
-    }
-    return false;
-  })
+export const ofType = (...keys) => source => source.pipe(
+  filter(({ type }) => keys.some(matchesType(type)))
 );
