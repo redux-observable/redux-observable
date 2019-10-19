562f93a90343d4a2aa1ffc7f4c272fd72510e273
Jay Phelps <hello@jayphelps.com>
2019-10-18 20:34:27 -0400

    chore(examples): Remove examples/ directory because they are outdated and not always idiomatic

--- examples/navigation/src/containers/App.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const App = ({ children }) => <div>{children}</div>
-
-export default App
--- examples/redux-observable-cancellable-counter/js/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import { counter, countdown } from './counter'
-
-const rootReducer = combineReducers({
-  countdown,
-  counter
-})
-
-export default rootReducer
